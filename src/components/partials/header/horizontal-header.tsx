0974e4c61fda51fca35c3c6a3fe3299144a3fbf6
VictorHOgawa <victor.ogawa@gmail.com>
2025-07-01 15:20:36 -0300

    refactor(feature): route logo header

--- src/components/partials/header/horizontal-header.tsx
+++ src/components/partials/header/horizontal-header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function HorizontalHeader() {
   return (
     <div className="flex items-center">
-      <Link href="#" className="text-primary flex items-center">
+      <Link href="/" className="text-primary flex items-center">
         <Image
           src="/logo/logo.png"
           alt="logo"
